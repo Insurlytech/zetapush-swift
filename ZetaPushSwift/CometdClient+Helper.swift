e27a5031a9b1b997198b38859f1851627b5bce99
Anthony Guiguen <anthony@insurlytech.com>
2019-04-24 07:37:57 +0200

    [R] Refactoring - Step 4

--- ZetaPushSwift/CometdClient+Helper.swift
+++ ZetaPushSwift/CometdClient+Helper.swift
@@ -1,26 +1,24 @@
 //
 //  CometdClient+Helper.swift
-//  ZetaPushSwift
+//  ZetaPush
 //
-//  Created by Morvan Mikaël on 23/03/2017.
-//  Copyright © 2017 ZetaPush. All rights reserved.
+//  Created by Leocare on 19/04/2019.
+//  Copyright © 2019 Leocare. All rights reserved.
 //
 // Adapted from https://github.com/hamin/FayeSwift
 
 import Foundation
 
 public extension CometdClient {
-    
-    // MARK: Helper
-    
-    ///  Validate whatever a subscription has been subscribed correctly
-    public func isSubscribedToChannel(_ channel:String) -> Bool {
-        return self.openSubscriptions.contains { $0.subscriptionUrl == channel }
-    }
-    
-    ///  Validate cometd transport is connected
-    public func isTransportConnected() -> Bool {
-        return self.transport!.isConnected()
-    }
+  // MARK: Helper
+  ///  Validate whatever a subscription has been subscribed correctly
+  public func isSubscribedToChannel(_ channel: String) -> Bool {
+    return self.openSubscriptions.contains { $0.subscriptionUrl == channel }
+  }
+  
+  ///  Validate cometd transport is connected
+  public func isTransportConnected() -> Bool {
+    return self.transport?.isConnected() ?? false
+  }
 }
 
--- ZetaPushSwift/CometdClient+Parsing.swift
+++ ZetaPushSwift/CometdClient+Parsing.swift
@@ -1,9 +1,9 @@
 //
 //  CometdClient+Parsing.swift
-//  ZetaPushSwift
+//  ZetaPush
 //
-//  Created by Morvan Mikaël on 23/03/2017.
-//  Copyright © 2017 ZetaPush. All rights reserved.
+//  Created by Leocare on 19/04/2019.
+//  Copyright © 2019 Leocare. All rights reserved.
 //
 // Adapted from https://github.com/hamin/FayeSwift
 
@@ -11,126 +11,112 @@ import Foundation
 import SwiftyJSON
 
 extension CometdClient {
-    
-    // MARK:
-    // MARK: Parsing
-    
-    func parseCometdMessage(_ messages: [JSON]) {
-        messages.forEach { (message) in
-            if let channel = message[Bayeux.Channel.rawValue].string {
-                log.verbose("parseCometdMessage \(channel)")
-                log.verbose(message)
-                
-                // Handle Meta Channels
-                if let metaChannel = BayeuxChannel(rawValue: channel) {
-                    switch(metaChannel) {
-                    case .Handshake:
-                        self.cometdClientId = message[Bayeux.ClientId.rawValue].stringValue
-                        if message[Bayeux.Successful.rawValue].int == 1 {
-                            if message[Bayeux.Ext.rawValue] != JSON.null {
-                                let ext : AnyObject = message[Bayeux.Ext.rawValue].object as AnyObject
-                                self.delegate?.handshakeSucceeded(self, handshakeDict: ext as! NSDictionary)
-                            }
-                            self.cometdConnected = true;
-                            self.connect()
-                            self.subscribeQueuedSubscriptions()
-                        } else {
-                            self.delegate?.handshakeFailed(self)
-                            self.cometdConnected = false;
-                            self.transport?.closeConnection()
-                            self.delegate?.disconnectedFromServer(self)
-                        }
-                    case .Connect:
-                        let advice = message[Bayeux.Advice.rawValue];
-                        let successful = message[Bayeux.Successful.rawValue];
-                        let reconnect = advice[BayeuxAdvice.Reconnect.rawValue].stringValue
-                        if successful.boolValue {
-                            if (reconnect == BayeuxAdviceReconnect.Retry.rawValue) {
-                                self.cometdConnected = true;
-                                self.delegate?.connectedToServer(self)
-                                self.connect()
-                            } else {
-                                self.cometdConnected = false;
-                            }
-                        } else {
-                            
-                            self.cometdConnected = false;
-                            self.transport?.closeConnection()
-                            self.delegate?.disconnectedFromServer(self)
-                            if (reconnect == BayeuxAdviceReconnect.Handshake.rawValue) {
-                                self.delegate?.disconnectedAdviceReconnect(self)
-                            }
-                        }
-                    case .Disconnect:
-                        if message[Bayeux.Successful.rawValue].boolValue {
-                            self.cometdConnected = false;
-                            self.transport?.closeConnection()
-                            self.delegate?.disconnectedFromServer(self)
-                        } else {
-                            self.cometdConnected = false;
-                            self.transport?.closeConnection()
-                            self.delegate?.disconnectedFromServer(self)
-                        }
-                    case .Subscribe:
-                        if let success = message[Bayeux.Successful.rawValue].int, success == 1 {
-                            if let subscription = message[Bayeux.Subscription.rawValue].string {
-                                _ = removeChannelFromPendingSubscriptions(subscription)
-                                
-                                self.openSubscriptions.append(CometdSubscriptionModel(subscriptionUrl: subscription, clientId: cometdClientId))
-                                self.delegate?.didSubscribeToChannel(self, channel: subscription)
-                            } else {
-                                log.warning("Cometd: Missing subscription for Subscribe")
-                            }
-                        } else {
-                            // Subscribe Failed
-                            if let error = message[Bayeux.Error.rawValue].string,
-                                let subscription = message[Bayeux.Subscription.rawValue].string {
-                                _ = removeChannelFromPendingSubscriptions(subscription)
-                                
-                                self.delegate?.subscriptionFailedWithError(
-                                    self,
-                                    error: subscriptionError.error(subscription: subscription, error: error)
-                                )
-                            }
-                        }
-                    case .Unsubscibe:
-                        if let subscription = message[Bayeux.Subscription.rawValue].string {
-                            _ = removeChannelFromOpenSubscriptions(subscription)
-                            self.delegate?.didUnsubscribeFromChannel(self, channel: subscription)
-                        } else {
-                            log.warning("Cometd: Missing subscription for Unsubscribe")
-                        }
-                    }
-                } else {
-                    // Handle Client Channel
-                    if self.isSubscribedToChannel(channel) {
-                        if message[Bayeux.Data.rawValue] != JSON.null {
-                            let data: AnyObject = message[Bayeux.Data.rawValue].object as AnyObject
-                            
-                            if let channelBlock = self.channelSubscriptionBlocks[channel] {
-                                for channel in channelBlock {
-                                    channel.callback!(data as! NSDictionary)
-                                }
-                            } else {
-                                log.warning("Cometd: Failed to get channel block for : \(channel)")
-                            }
-                            
-                            self.delegate?.messageReceived(
-                                self,
-                                messageDict: data as! NSDictionary,
-                                channel: channel
-                            )
-                        } else {
-                            log.warning("Cometd: For some reason data is nil for channel: \(channel)")
-                        }
-                    } else {
-                        log.warning("Cometd: Weird channel that not been set to subscribed: \(channel)")
-                    }
-                }
-            } else {
-                log.warning("Cometd: Missing channel for \(message)")
-            }
+  // MARK: Parsing
+  func parseCometdMessage(_ messages: [JSON]) {
+    messages.forEach { (message) in
+      guard let channel = message[Bayeux.Channel.rawValue].string else {
+        log.warning("Cometd: Missing channel for \(message)")
+        return
+      }
+      log.verbose("parseCometdMessage \(channel)")
+      log.verbose(message)
+      
+      // Handle Meta Channels
+      if let metaChannel = BayeuxChannel(rawValue: channel) {
+        parseMetaChannel(metaChannel, for: message)
+      } else {
+        // Handle Client Channel
+        guard isSubscribedToChannel(channel) else {
+          log.warning("Cometd: Weird channel that not been set to subscribed: \(channel)")
+          return
         }
+        guard let data = message[Bayeux.Data.rawValue].object as? NSDictionary else {
+          log.warning("Cometd: For some reason data is nil for channel: \(channel)")
+          return
+        }
+        
+        if let channelBlock = channelSubscriptionBlocks[channel] {
+          for channel in channelBlock {
+            channel.callback?(data)
+          }
+        } else {
+          log.warning("Cometd: Failed to get channel block for : \(channel)")
+        }
+        delegate?.messageReceived(self, messageDict: data, channel: channel)
+      }
     }
+  }
+  
+  private func parseMetaChannel(_ metaChannel: BayeuxChannel, for message: JSON) {
+    switch(metaChannel) {
+    case .Handshake:
+      cometdClientId = message[Bayeux.ClientId.rawValue].stringValue
+      if message[Bayeux.Successful.rawValue].int == 1 {
+        if let ext = message[Bayeux.Ext.rawValue].object as? NSDictionary {
+          delegate?.handshakeSucceeded(self, handshakeDict: ext)
+        }
+        cometdConnected = true
+        connect()
+        subscribeQueuedSubscriptions()
+      } else {
+        delegate?.handshakeFailed(self)
+        cometdConnected = false
+        transport?.closeConnection()
+        delegate?.disconnectedFromServer(self)
+      }
+    case .Connect:
+      let advice = message[Bayeux.Advice.rawValue]
+      let successful = message[Bayeux.Successful.rawValue]
+      let reconnect = advice[BayeuxAdvice.Reconnect.rawValue].stringValue
+      if successful.boolValue {
+        if reconnect == BayeuxAdviceReconnect.Retry.rawValue {
+          cometdConnected = true
+          delegate?.connectedToServer(self)
+          connect()
+        } else {
+          cometdConnected = false
+        }
+      } else {
+        cometdConnected = false
+        transport?.closeConnection()
+        delegate?.disconnectedFromServer(self)
+        if reconnect == BayeuxAdviceReconnect.Handshake.rawValue {
+          delegate?.disconnectedAdviceReconnect(self)
+        }
+      }
+    case .Disconnect:
+      if message[Bayeux.Successful.rawValue].boolValue {
+        cometdConnected = false
+        transport?.closeConnection()
+        delegate?.disconnectedFromServer(self)
+      } else {
+        cometdConnected = false
+        transport?.closeConnection()
+        delegate?.disconnectedFromServer(self)
+      }
+    case .Subscribe:
+      if let success = message[Bayeux.Successful.rawValue].int, success == 1 {
+        if let subscription = message[Bayeux.Subscription.rawValue].string {
+          removeChannelFromPendingSubscriptions(subscription)
+          
+          openSubscriptions.append(CometdSubscriptionModel(subscriptionUrl: subscription, clientId: cometdClientId))
+          delegate?.didSubscribeToChannel(self, channel: subscription)
+        } else {
+          log.warning("Cometd: Missing subscription for Subscribe")
+        }
+      } else if let error = message[Bayeux.Error.rawValue].string,
+        let subscription = message[Bayeux.Subscription.rawValue].string { // Subscribe Failed
+        removeChannelFromPendingSubscriptions(subscription)
+        delegate?.subscriptionFailedWithError(self, error: subscriptionError.error(subscription: subscription, error: error))
+      }
+    case .Unsubscibe:
+      if let subscription = message[Bayeux.Subscription.rawValue].string {
+        removeChannelFromOpenSubscriptions(subscription)
+        delegate?.didUnsubscribeFromChannel(self, channel: subscription)
+      } else {
+        log.warning("Cometd: Missing subscription for Unsubscribe")
+      }
+    }
+  }
 }
 
