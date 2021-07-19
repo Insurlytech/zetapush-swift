0a9ef341abe054baabe504a22c314b18ba09e934
Steven Watremez <steven@insurlytech.com>
2021-07-19 16:46:32 +0200

    [func] remove commented code

--- Sources/ZetaPushNetwork/Keychain/Factories/ZPKeychainOperationsFactory.swift
+++ Sources/ZetaPushNetwork/Keychain/Factories/ZPKeychainOperationsFactory.swift
@@ -11,8 +11,6 @@ import Foundation
 enum ZPKeychainOperationsFactory {
   static func createAttributes(to operation: ZPKeychainOperationType, _ item: ZPKeychainItem) -> CFDictionary {
     switch item {
-//    case let .password(account, service):
-//      return ZPKeychainPasswordAttributesFactory.createAttributes(for: operation, associtedTo: account, and: service.rawValue)
     case let .sandboxId(label, server), let .token(label, server), let .publicToken(label, server):
       return ZPKeychainPasswordAttributesFactory.createAttributes(for: operation, associatedTo: label, and: server)
     }
@@ -20,8 +18,6 @@ enum ZPKeychainOperationsFactory {
   
   static func createQuery(to operation: ZPKeychainOperationType, _ item: ZPKeychainItem) -> CFDictionary {
     switch item {
-//    case let .password(account, service):
-//      return ZPKeychainPasswordQueryFactory.createQuery(for: operation, associtedTo: account, and: service.rawValue)
     case let .sandboxId(label, server), let .token(label, server), let .publicToken(label, server):
       return ZPKeychainPasswordQueryFactory.createQuery(for: operation, associatedTo: label, and: server)
     }
